d9808757d9cf0dea4ed333886bb7379467f5a1a8
ByalykT01 <byalykt@hotmail.com>
2023-11-24 16:20:51 +0000

    first version of insertion sort

--- src/SortingAlgorithms/InsertionSort.tsx
+++ src/SortingAlgorithms/InsertionSort.tsx
@@ -12,38 +12,28 @@ async function insertionSort(oldArr, setArr) {
     });
 
     if (pauseSorting) {
-      pauseSorting = false; // Reset the flag
-      i = arr.length - 1; // Stop the loop after the current iteration
+      pauseSorting = false; 
+      i = arr.length - 1;
       continue;
     }
 
     const currentElement = arr[i];
     let j = i - 1;
 
-    arr.forEach((element) => {
-      element.isActive = false;
-    });
-
     if (arr[i]) arr[i].isActive = true;
 
-    for (j = i - 1; j >= 0 && arr[j].id > currentElement.id; j--) {
-      if(arr[j+1])
-      arr[j+1].isActive = true;
-      if(arr[j])
-        arr[j].isActive = false;
+    for (j; j >= 0 && arr[j].id > currentElement.id; j--) {
       arr[j + 1] = arr[j];
       setArr([...arr]);
-
     }
-
     arr[j + 1] = currentElement;
-    if (arr[j + 1]) arr[j + 1].isActive = false; // Reset isActive for the element being placed in its sorted position
+    if (arr[j]) arr[j].isActive = false
 
     setArr([...arr]);
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await new Promise((resolve) => setTimeout(resolve, 20));
 
     if (arr[j + 1]) {
-      arr[j + 1].isActive = false; // Reset isActive after the update
+      arr[j + 1].isActive = false;
     }
 
     arr.forEach((element) => {
@@ -58,75 +48,3 @@ async function insertionSort(oldArr, setArr) {
 
 export { insertionSort };
 
-// const insertionSort = (
-//   arr,
-//   setArrayHandler,
-// ) => {
-//   let newArr = [...arr];
-//   let currentIndex = 1;
-
-//   subscribe("stopSort", () => {
-//     if (intervalId) {
-//       clearInterval(intervalId);
-//     }
-//   });
-
-//   const intervalId = setInterval(() => {
-//     const currentElement = newArr[currentIndex];
-//     let j = currentIndex - 1;
-
-//     if (newArr[currentIndex]) newArr[currentIndex].isActive = false;
-
-//     newArr.forEach((element) => {
-//       element.isActive = false;
-//     });
-
-//     newArr[j].isActive = true;
-//     newArr[currentIndex].isActive = true;
-
-//     while (j >= 0 && parseInt(newArr[j].id) > parseInt(currentElement.id)) {
-//       newArr[j + 1] = newArr[j];
-//       j--;
-//       setArrayHandler([...newArr]);
-//     }
-
-//     newArr[j + 1] = currentElement;
-//     setArrayHandler([...newArr]);
-
-//     currentIndex++;
-
-//     if (currentIndex >= newArr.length) {
-//       clearInterval(intervalId);
-//       console.log(j + 1);
-
-//       if (newArr[j + 1]) newArr[j + 1].isActive = false;
-
-//       if (newArr[newArr.length - 1]) newArr[newArr.length - 1].isActive = false;
-
-//       setArrayHandler([...newArr]);
-
-//       let currentSortedIndex = 0;
-
-//       subscribe("stopSort", () => {
-//         if (sortedIntervalId) {
-//           clearInterval(sortedIntervalId);
-//         }
-//       });
-
-//       const sortedIntervalId = setInterval(() => {
-//         if (newArr[currentSortedIndex + 1]) {
-//           newArr[currentSortedIndex].isSorted = true;
-//           newArr[currentSortedIndex + 1].isActive = true;
-//           currentSortedIndex++;
-//           setArrayHandler([...newArr]);
-//         } else {
-//           newArr[currentSortedIndex].isSorted = true;
-//           setArrayHandler([...newArr]);
-//           clearInterval(sortedIntervalId);
-//         }
-//       }, 1);
-//     }
-//   }, 100);
-// };
-
-// export { insertionSort };
