0c1fb653d1408fdf11730ea91ccf8047d04645e2
ByalykT01 <byalykt@hotmail.com>
2023-11-25 17:41:47 +0000

    Implementation and visualization of quick sort

--- src/SortingAlgorithms/QuickSort.ts
+++ src/SortingAlgorithms/QuickSort.ts
@@ -5,18 +5,69 @@ async function quickSort(oldArr, setArr) {
   publish("sortingStarted", null);
 
   let arr = [...oldArr];
-  let arrLength = arr.length;
+  let pauseSorting: boolean = false;
 
-  const pivotChooser = (arr) => {
-    //Implementation of medium-of-three pivot pick scheme
-    const pivotChooserArr = [arr[0], arr[Math.floor((arrLength - 1) / 2)] ,arr[arrLength - 1]]
-    
-    const pivot = pivotChooserArr.sort()[1]
-    return pivot
+  subscribe("pauseSorting", () => {
+    pauseSorting = true;
+  });
+
+  async function partition(start, end) {
+    let pivotIndex = start;
+    let pivotValue = arr[end];
+
+    for (let i = start; i < end; i++) {
+      if (arr[i].id < pivotValue.id) {
+        [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
+        pivotIndex++;
+      }
+    }
+
+    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]];
+
+    return pivotIndex;
   }
 
+  async function quickSortHelper(start, end) {
+    if (start >= end) {
+      return;
+    }
+
+    arr[start].isActive = true;
+    arr[end].isActive = true;
+
+    setArr([...arr]);
+    await delay(20);
+
+    let index = await partition(start, end);
+
+    arr[start].isActive = false;
+    arr[end].isActive = false;
+
+    await delay(20);
+
+    if (pauseSorting) {
+      publish("sortingStopped", arr);
+      setArr(arr);
+      return;
+    }
+
+    await quickSortHelper(start, index - 1);
+    await quickSortHelper(index + 1, end);
+  }
+
+  await quickSortHelper(0, arr.length - 1);
   
+  arr.forEach((element) => {
+    element.isActive = false;
+  });
+
+  console.log("Sorting has ended.");
+
+  if (!pauseSorting) {
+    publish("sortingEnded", arr);
+  }
 
   return arr;
 }
+
 export { quickSort };
